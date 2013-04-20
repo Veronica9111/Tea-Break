77169ed68d83ce78d57d52dc2d7166eab8ac2483
Veronica <anjjellia@gmail.com>
2013-04-20 23:14:36 +0800

    Add project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeaBreak::Application.config.secret_token = '72c1deb026746b285adf668f59312a05c7097c70ddc8cd90fb9843614e130c7c33d141270cca05600bbc104f64253a69fb03cb43aaff7885a2d413bc02196d0b'
